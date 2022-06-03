9f1dbba7fb73458d3340e126c994c0c19353dd8f
Darko Kuzmanović <43079580+Kuzmanovich@users.noreply.github.com>
2022-06-03 22:46:20 +0200

    Priprema za pismeni

--- web/main.js
+++ web/main.js
@@ -18,4 +18,14 @@ function myTest() {
 
 }
 
+var div = document.createElement("div");
+div.id = "div_id";
+div.className = "div_class";
+div.style = "background-color: green; ";
+div.style.width = "100px";
+div.style.height = "100px";
+div.innerHTML = Korisnik.ime + " " + Korisnik.prezime;
 
+var profileDiv = document.getElementById("profile");
+
+profileDiv.appendChild(div);
